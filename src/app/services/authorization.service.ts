121303866fa27a69b8ae24c8ae440586be9675b5
Anders Jakobsen <andersjkbsn@gmail.com>
2018-11-23 13:49:36 +0100

    Further work to auth

--- src/app/services/authorization.service.ts
+++ src/app/services/authorization.service.ts
@@ -1,38 +1,57 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import * as jwt_decode from 'jwt-decode';
+import { tap, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorizationService {
   private path = `${environment.apiUrl}/authorization`;
-  // private loggedIn = new BehaviorSubject<boolean>(this.getIsLoggedIn());
+  private loggedIn = new BehaviorSubject<boolean>(this.getIsLoggedIn());
 
   constructor(private http: HttpClient) {}
 
-  login(email: String, password: String): Observable<Boolean> {
-    const subscription = new Subject<Boolean>();
-
-    this.http.post(`${this.path}/login`, {email, password}).subscribe(data => {
-      this.setSession(data.token);
-
-      subscription.next(this.getIsLoggedIn());
-    }, error => {
-      subscription.next(false);
-    });
-
-    return subscription;
+  login(email: String, password: String) {
+    return this.http.post(`${this.path}/login`, { email, password })
+    .pipe(
+      tap(response => {
+        this.setSession(response);
+        this.loggedIn.next(true);
+      }),
+      shareReplay()
+    );
   }
 
-  setSession(token: String): void {
-    const decoded = jwt_decode(token);
-    console.log(decoded);
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expires');
+
+    this.loggedIn.next(false);
+
+    return this.http.get(`${this.path}/logout`);
+  }
+
+  setSession(result: any): void {
+    localStorage.setItem('token', result.token);
+    localStorage.setItem('expires', JSON.stringify(result.expires));
+
+    console.log(localStorage.getItem('expires'));
+  }
+
+  get isLoggedIn() {
+    return this.loggedIn.asObservable();
   }
 
   getIsLoggedIn(): Boolean {
+    const expiration = localStorage.getItem('expires');
+    if (!expiration) {
+      return false;
+    } else {
+      return new Date().getTime() < parseInt(expiration);
+    }
     return true;
   }
 }
