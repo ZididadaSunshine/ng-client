d713fcc6ea46dd20cfdeb1c6f452e6164019f2be
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2018-11-28 01:50:44 +0100

    Added a logo

--- src/app/layouts/authenticated/authenticated-layout.component.ts
+++ src/app/layouts/authenticated/authenticated-layout.component.ts
@@ -2,17 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-authenticated-layout',
   templateUrl: './authenticated-layout.component.html',
   styleUrls: ['./authenticated-layout.component.css']
 })
-export class AuthenticatedLayoutComponent {
+export class AuthenticatedLayoutComponent implements OnInit {
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private matIconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.matIconRegistry.addSvgIcon('logo', this.sanitizer.bypassSecurityTrustResourceUrl('/assets/img/logo.svg'))
+  }
 }
